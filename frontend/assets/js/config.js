63dfd17f95e129a520ddad101bd80db5ca25c65a
Ayan-Paul <paul.ayan@outlook.com>
2026-03-08 11:47:29 +0530

    Add API url

--- frontend/assets/js/config.js
+++ frontend/assets/js/config.js
@@ -1,11 +1,14 @@
 (function (global) {
+  var runtimeConfig = global.__APP_CONFIG__ || {};
   var isLocalHost = ["127.0.0.1", "localhost"].indexOf(window.location.hostname) !== -1;
   var defaultApiBase = isLocalHost
     ? "http://127.0.0.1:8000/api/v1"
-    : "https://todo-api.onrender.com/api/v1";
+    : "";
+  var apiBaseUrl = runtimeConfig.API_BASE_URL || defaultApiBase;
+  var tokenKey = runtimeConfig.TOKEN_KEY || "todo_app_access_token";
 
   global.APP_CONFIG = {
-    API_BASE_URL: defaultApiBase,
-    TOKEN_KEY: "todo_app_access_token"
+    API_BASE_URL: apiBaseUrl,
+    TOKEN_KEY: tokenKey
   };
 })(window);
